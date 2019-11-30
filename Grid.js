9093bbc8bc584f08a0205d6edebef8823ddb7895
Aniko Magyar <44470375+mofolade@users.noreply.github.com>
2019-11-30 14:12:31 +0100

    Grid.js
    Rörelse vid piltagenterna

---
+++ Grid.js
@@ -0,0 +1,189 @@
+import Tile from './Tile.js'
+import Maps from './Maps.js'
+
+export default{
+    components: {
+        Tile,
+        Maps,
+    },
+    props: {
+        size: Number,
+    },
+    template: `
+    <div class="grid">
+      <img
+        v-for="(tile, i) in flatTiles"
+        :key="'tile' + i + tile.position.x + tile.position.y"
+        class="tile" 
+        :src="tilePicture(tile.tileState)">
+    </div>
+    `,
+    data() {
+        return {
+            playerPosition: [7,3], //x,y
+            tiles: [],
+            map1:[
+                ['W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'G' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W', 'W' ,'W' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'G' ,'D' ,'D' ,'D' ,'D' ,'D' ,'G' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'G' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'G' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W', 'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'D' ,'W'],
+                ['W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W' ,'W'],
+
+            ]
+        }
+    },
+    methods: {
+        keyHandler(e) {
+            /**
+              37 - left
+              39 - right
+              38 - up
+              40 - down
+             */
+            if (e.keyCode === 37) {
+                this.handleKeyLeft(e);
+            } else if (e.keyCode === 39) {
+                this.handleKeyRight(e);
+            }else if (e.keyCode === 38) {
+              this.handleKeyUp(e);
+            } else if (e.keyCode === 40) {
+              this.handleKeyDown(e);
+            }
+        },
+        handleKeyUp(e) {
+            //canMoveTo - true -> move y-1
+            if(this.canMoveTo(this.playerPosition[0],this.playerPosition[1]-1) == true){
+                this.move(e);
+                console.log(this.playerPosition[0]+','+this.playerPosition[1]);
+            }
+            else{
+                console.log('Up: not possible');
+            }
+            
+            // map1 update
+            // refresh
+            
+        },
+        handleKeyDown(e){
+            //canMoveTo - true -> move y+1
+            if(this.canMoveTo(this.playerPosition[0],this.playerPosition[1]+1) == true){
+                this.move(e);
+                console.log(this.playerPosition[0]+','+this.playerPosition[1]);
+            }
+            else{
+                console.log('Down: not possible');
+            }    
+        },
+        handleKeyLeft(e){
+            //canMoveTo - true -> move x-1
+            if(this.canMoveTo(this.playerPosition[0]-1,this.playerPosition[1]) == true){
+                this.move(e);
+                console.log(this.playerPosition[0]+','+this.playerPosition[1]);
+            }
+            else{
+                console.log('Left: not possible');
+            }    
+        },
+        handleKeyRight(e){
+            //canMoveTo - true -> move x+1
+            if(this.canMoveTo(this.playerPosition[0]+1,this.playerPosition[1]) == true){
+                this.move(e);
+                console.log(this.playerPosition[0]+','+this.playerPosition[1]);
+            }
+            else{
+                console.log('Right: not possible');
+            }    
+        },
+        canMoveTo(x,y){
+            //inside the map
+            if(x < 0 ||x > this.size || y < 0 || y >= this.size){
+                return false;
+            }
+            //you can only go down the road
+            if(this.map1[y][x] !== 'D' && this.map1[y][x] !== 'X'){
+                console.log(this.map1[y][x]);
+                return false;
+            }
+            return true;
+        },
+        playerMove(e){
+            this.map1[this.playerPosition[1]][this.playerPosition[0]] = 'X';
+            if(e.keyCode === 37){
+                //moveLeft x-1
+                this.playerPosition[0]=this.playerPosition[0]-1;
+            }
+            else if(e.keyCode === 39){
+            //moveRight x+1
+                this.playerPosition[0]=this.playerPosition[0]+1;
+            }          
+            else if(e.keyCode === 38){
+            //moveUp y-1'
+                this.playerPosition[1]=this.playerPosition[1]-1;
+            }else if(e.keyCode === 40){
+            //moveDown y+1
+                this.playerPosition[1]=this.playerPosition[1]+1;
+            }
+            //new position on the map            
+            this.map1[this.playerPosition[1]][this.playerPosition[0]] = 'P';    
+        },
+        randomMove(x,y){
+            //canMoveTo
+            //Move
+        },
+        fillTiles() {
+            for(let row = 0; row < this.size; row++){
+                this.tiles[row] = [];
+                for(let col = 0; col < this.size; col++){
+                    if( this.map1[row][col] === 'D'){
+                        this.tiles[row].push(new Tile('dirt', row, col, false))
+                    }else if(this.map1[row][col] === 'P'){
+                        this.tiles[row].push(new Tile('playerModel', row, col, false))
+                    }else if(this.map1[row][col] === 'W'){
+                        this.tiles[row].push(new Tile('wall', row, col, false))
+                    }else if(this.map1[row][col] === 'G'){
+                        this.tiles[row].push(new Tile('gem', row, col, false))
+                    }
+                    else if(this.map1[row][col] === 'X'){
+                        this.tiles[row].push(new Tile('x', row, col, false))
+                    }
+                }
+            }
+        },
+        tilePicture(tileState){
+            let pictureLocation = "img/" + tileState + ".png";
+            return pictureLocation;
+        },
+        setKeyHandler(e) {            
+            window.addEventListener("keydown", this.keyHandler);
+        }
+    },
+    updatedPlayerPosition(x,y){
+        let player = new 
+        this.tiles()
+    },
+    computed: {
+        flatTiles() {
+            return this.tiles.flat();
+        }
+    },
+    created() {
+        this.map1[this.playerPosition[1]][this.playerPosition[0]] = 'P';
+        this.fillTiles();
+        this.setKeyHandler();
+    }
+}
