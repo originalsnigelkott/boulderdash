f7dc2ceec7dc3938a58f8808d78c628d129f8fb3
snigelkott <dahlstedt.per@gmail.com>
2019-12-04 12:35:38 +0100

    game over if player is hit by boulder
    Game is aborted but the GameOverLevel is not loading.

--- js/Store.js
+++ js/Store.js
@@ -12,9 +12,9 @@ const maps = [
 ];
 
 const currentLevel = {
-    title: [level1.title, level2.title],
-    mapSizeX: [level1.mapSizeX, level2.mapSizeX],
-    mapSizeY: [level1.mapSizeY, level2.mapSizeY],
+    title: [level1.title, level2.title, GameOverLevel.title],
+    mapSizeX: [level1.mapSizeX, level2.mapSizeX, GameOverLevel.mapSizeX],
+    mapSizeY: [level1.mapSizeY, level2.mapSizeY, GameOverLevel.mapSizeY],
     playerPosition: [level1.playerPosition, level2.playerPosition],
     enemyPosition: [level1.enemyPosition, level2.enemyPosition],
     boulderPositions: [level1.boulderPositions, level2.boulderPositions]
