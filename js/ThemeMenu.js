30838416901c479893c19faec4daae65b2e9a368
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2019-12-06 11:44:07 +0100

    Added a themes menu to the Grid

---
+++ js/ThemeMenu.js
@@ -0,0 +1,34 @@
+export default{
+    template:`
+        <div id="gallery">
+            <h1> Click to choose a theme</h1>
+            <div class="styleinfo">
+                <div class="imgbox">                
+                    <img src="/img/style_default.jpg"
+                    @click='changeThemeToDefault' />
+                </div>
+            </div>
+                <div class="styleinfo">
+                <div class="imgbox">                
+                    <img src="/img/style_frozen.jpg"
+                    @click='changeThemeToFrozen' />
+                </div>
+            </div>
+        </div>
+    `,
+    data(){
+        return{
+            theme: ''
+        }
+    },
+    methods:{
+        changeThemeToFrozen(){
+            this.theme = 'e'
+            this.$emit('changeTheme', this.theme)
+        },
+        changeThemeToDefault(){
+            this.theme = 'd'
+            this.$emit('changeTheme', this.theme)
+        }
+    }
+}
