ef0675b7901103a76f4f12b8553890752d4de9f4
Marcus Sandberg <marcus.a.sandberg@outlook.com>
2019-12-10 20:14:04 +0100

    Timer fix

--- js/Timer.js
+++ js/Timer.js
@@ -1,8 +1,8 @@
 export default{
     template:`
         <div id='gameTimer'>
-            <h3 v-if='timeLimit == 45'> Ready </h3>
-            <h3 v-if='timeLimit < 45 && timeLimit != 0'> {{timeLimit}} </h3>
+            <h3 v-if='timeLimit == 65'> Ready </h3>
+            <h3 v-if='timeLimit < 65 && timeLimit != 0'> {{timeLimit}} </h3>
             <h3 v-if='timeLimit == 0'> Out of time </h3>
         </div>
     `,
